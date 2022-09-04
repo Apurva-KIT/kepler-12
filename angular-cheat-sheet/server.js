92676738ef71ea420dc4423381fb1a3799cbc940
Apurva Yadav <Apurva-KIT@users.noreply.github.com>
2022-09-04 17:01:40 +0530

    code modified

--- angular-cheat-sheet/server.js
+++ angular-cheat-sheet/server.js
@@ -6,11 +6,11 @@ const app = express();
 
 console.log('==>',__dirname);
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/kepler-12/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/kepler-12/angular-cheat-sheet'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/kepler-12/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/kepler-12/angular-cheat-sheet/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
